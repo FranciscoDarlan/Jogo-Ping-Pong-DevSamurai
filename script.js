53709e5485907d687cbdd54685b106ac12d7ee82
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2024-03-11 20:54:13 -0300

    colocando word para realiza teste

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const canvasE1 = document.querySelector("canvas"),
-canvasCtx = canvasE1.getContext("2d");
-const lineWidth = 15
+    canvasCtx = canvasE1.getContext("2d");
+const lineWidth = 15;
 
 function setup() {
-canvasE1.width = canvasCtx.width = window.innerWidth;
-canvasE1.height = canvasCtx.heigth = window.innerHeight;
+    canvasE1.width = canvasCtx.width = window.innerWidth;
+    canvasE1.height = canvasCtx.heigth = window.innerHeight;
 }
 
 function draw() {
-canvasCtx.fillStyle = "#286047";
-canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    canvasCtx.fillStyle = "#286047";
+    canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-canvasCtx.fillStyle = "#ffffff"
+    canvasCtx.fillStyle = "#ffffff";
+    
+    const x = window.innerWidth / 2 - lineWidth / 2;
+    const y = 0;
+    const w = lineWidth;
+    const h = window.innerHeight;
 
-const x = window.innerWidth /2 - lineWidth /2 
-const y = 0
-const w = lineWidth
-const h = window.innerHeight
-
-
-canvasCtx.fillRect(x, y, w, h);
+    canvasCtx.fillRect(x, y, w, h);
 }
 
-setup()
-draw()
+setup();
+draw();
